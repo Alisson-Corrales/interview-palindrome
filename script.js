ac79e423f6d7b25d53435db72dbc4d8b6e2ffa8d
Alisson Corrales <Acorra435@west-mec.local>
2021-04-08 18:26:47 -0700

    started ac 4/8/2021

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+let words = 
+`'bakamitai', 'alien', 'kayak'`;
+//let letters = words.split("")
+//for(let i in letters){
+//    letters[i] = letters[i].split("'").join("")
+//}
+
+//function reverseWords(){
+    let letters = words.split("")
+    //console.log(letters)
+    let reversed = letters.reverse();
+    console.log(reversed)
+    //let
+//}
